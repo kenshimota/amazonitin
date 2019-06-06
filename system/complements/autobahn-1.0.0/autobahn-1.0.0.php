bab04535e12f0f7be1b5c86b7dec73566a84b6c1
Erik de Jesus Mota Galindo <kenshimota@gmail.com>
2019-06-06 17:43:18 -0430

    adding autobahn complement to use for conections websockets e n Rachet Websocket

---
+++ system/complements/autobahn-1.0.0/autobahn-1.0.0.php
@@ -0,0 +1,10 @@
+<?php
+
+// clase que se encargara de la instalacion del paquete jquery
+class autobahn{
+
+	public static $type_complements = "Scripts";
+	public static $files_path = [
+		"autobahn.js" => "script"
+	];
+}
